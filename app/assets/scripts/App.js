4743ffac071c7d4894f738a208d9d825c7298100
fadi-tr <realmadridlover82@gmail.com>
2022-05-31 15:30:28 +0300

    Added client area feature.

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -3,10 +3,11 @@
  import MobileMenu from './modules/MobileMenu'
  import RevealOnScroll from './modules/RevealOnScroll'
  import StickyHeader from './modules/StickyHeader'
+ import ClientArea from './modules/ClientArea'
 
 
- //import {set, unset } from 'lodash'
 
+ new ClientArea()
  new StickyHeader()
  new RevealOnScroll(document.querySelectorAll(".feature-item"), 75)
  new RevealOnScroll(document.querySelectorAll(".testimonial"), 60)
